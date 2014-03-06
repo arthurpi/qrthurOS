bf89b2483deb84a3657835c0f1eb0908dcb853f3
qrthur <arthur.sfez@gmail.com>
2014-03-06 16:40:53 +0000

    runOS.sh generic home path

--- runOS.sh
+++ runOS.sh
@@ -2,6 +2,7 @@
 
 # Emulates qrthurOS with bochs
 
-# bochs
 bochs -f ./misc/bochsrc 'display_library: sdl'
-#/home/qrthur/srcs/bochs-2.6.2/bochs 'boot:a' 'floppya: 1_44=bin/floppyA, status=inserted'
+
+# Debugging
+$HOME/srcs/bochs-2.6.2/bochs -f ./misc/bochsrc 'display_library: sdl'
